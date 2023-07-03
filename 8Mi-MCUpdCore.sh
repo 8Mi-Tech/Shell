d64ec3653c014c41f0c319149ba67f0e6d951e81
8Mi_Yile <admin@8mi.tech>
2023-07-03 14:21:26 +0800

    support PaperMC Server

--- 8Mi-MCUpdCore.sh
+++ 8Mi-MCUpdCore.sh
@@ -6,17 +6,17 @@
 # !/bin/bash
 # source 8Mi-MCUpdCore.sh
 
-# jenkins_getLatestDownloadURL
+# jenkins_getLatestDLURL
 # $1 = * URL (example "https://ci.viaversion.com/job/ViaVersion-DEV")
 # $2 = * ArtifactID(default:0) (example 0)
-jenkins_getLatestDownloadURL(){
+jenkins_getLatestDLURL(){
     echo $1/lastSuccessfulBuild/artifact/`wget -qO- $1/lastSuccessfulBuild/api/json | jq -r ".artifacts[${2:-0}].relativePath"` >&1
 }
 
-# curseforge_getLatestDownloadURL
+# curseforge_getLatestDLURL
 # $1 = * App/Type/Name (example "minecraft/mc-mods/fabric-api")
 # $2 =   Game_Version (example "1.20.1")
-curseforge_getLatestDownloadURL(){
+curseforge_getLatestDLURL(){
     cf_json=`wget -qO- "https://api.cfwidget.com/"$1"?version="$2`
     cf_file_id=`echo $cf_json | jq '.download.id'`
     cf_file_name=`echo $cf_json | jq -r '.download.name'`
@@ -32,19 +32,35 @@ curseforge_getLatestDownloadURL(){
     fi
 }
 
-# modrinth_getLatestDownloadURL
+# modrinth_getLatestDLURL
 # $1 = * Project(Name/ID) (example "fabric-api" or "P7dR8mSH")
 # $2 =   GameVersion (example "1.20.1")
-modrinth_getLatestDownloadURL(){
+modrinth_getLatestDLURL(){
     if [ !$2 ]; then
     modrinth_gameversion=`wget -qO- https://api.modrinth.com/v2/tag/game_version | jq -r .[0].version`
     fi
     wget -qO- https://api.modrinth.com/v2/project/$1/version?game_versions=[%22${2:-$modrinth_gameversion}%22] | jq -r .[0].files[0].url >&1
 }
 
+# papermc_getLatestDLURL
+# $1 = * Name (Tips visit: https://api.papermc.io/v2/projects)
+# $2 =   GameVersion (example 1.20.1)
+#                    (if not set version, default get minecraft's latest version)
+papermc_getLatestDLURL() {
+    if [ `wget -qO- https://api.papermc.io/v2/projects | jq ".projects|index(\"$1\")"` != "null" ]; then
+        papermc_url="https://api.papermc.io/v2/projects/"$1
+        papermc_url=$papermc_url"/versions/"${2:-`wget -qO- $papermc_url | jq -r ".versions[-1]"`}
+        papermc_url=$papermc_url"/builds/"`wget -qO- $papermc_url | jq -r ".builds[-1]"`
+        papermc_url=$papermc_url"/downloads/"`wget -qO- $papermc_url | jq -r ".downloads.application.name"`
+        echo "$papermc_url" >&1
+    else
+        echo "papermc not have "$1 >&2
+    fi
+}
+
 # 8Mi_Download
 # $1 = * FileName (example FabricAPI.jar)
-# $2 =   DownloadURL(support STDIN) (example https://example.com/fabric-api/fabric-api-1.0-1.20.1.jar) 
+# $2 =   Download URL (support STDIN) (example https://example.com/fabric-api/fabric-api-1.0-1.20.1.jar) 
 8Mi_Download(){
     dl_url=`cat /dev/stdin`
     echo "[8Mi-MCUpdCore] 正在通过服务器"` echo ${2:-$dl_url} | awk -F'/' '{print $3}' `" 下载"$1
