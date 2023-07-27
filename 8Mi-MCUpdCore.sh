bb1f9cd613a4a33161e5687e451e1dffd622799a
8Mi_Yile <admin@8mi.tech>
2023-07-27 23:06:39 +0800

    8Mi-MCUpdCore.sh: Fix: About cURL Fix: cURL not slient

--- 8Mi-MCUpdCore.sh
+++ 8Mi-MCUpdCore.sh
@@ -13,14 +13,14 @@ wget --help | grep -q '\--show-progress' && WGET_PROGRESS_OPT="-q --show-progres
 # $1 = * URL (example "https://ci.viaversion.com/job/ViaVersion-DEV")
 # $2 = * ArtifactID(default:0) (example 0)
 jenkins_getLatestDLURL(){
-    echo $1/lastSuccessfulBuild/artifact/`wget -qO- $1/lastSuccessfulBuild/api/json | jq -r ".artifacts[${2:-0}].relativePath"` >&1
+    echo $1/lastSuccessfulBuild/artifact/`curl -s $1/lastSuccessfulBuild/api/json | jq -r ".artifacts[${2:-0}].relativePath"` >&1
 }
 
 # curseforge_getLatestDLURL
 # $1 = * App/Type/Name (example "minecraft/mc-mods/fabric-api")
 # $2 =   Game_Version (example "1.20.1")
 curseforge_getLatestDLURL(){
-    cf_json=`wget -qO- "https://api.cfwidget.com/"$1"?version="$2`
+    cf_json=`curl -s "https://api.cfwidget.com/"$1"?version="$2`
     cf_file_id=`echo $cf_json | jq '.download.id'`
     cf_file_name=`echo $cf_json | jq -r '.download.name'`
     if [ $cf_file_id ]; then
@@ -40,9 +40,9 @@ curseforge_getLatestDLURL(){
 # $2 =   GameVersion (example "1.20.1")
 modrinth_getLatestDLURL(){
     if [ !$2 ]; then
-    modrinth_gameversion=`wget -qO- https://api.modrinth.com/v2/tag/game_version | jq -r .[0].version`
+    modrinth_gameversion=`curl -s https://api.modrinth.com/v2/tag/game_version | jq -r .[0].version`
     fi
-    wget -qO- https://api.modrinth.com/v2/project/$1/version?game_versions=[%22${2:-$modrinth_gameversion}%22] | jq -r .[0].files[0].url >&1
+    curl -s https://api.modrinth.com/v2/project/$1/version?game_versions=[%22${2:-$modrinth_gameversion}%22] | jq -r .[0].files[0].url >&1
 }
 
 # papermc_getLatestDLURL
@@ -50,11 +50,11 @@ modrinth_getLatestDLURL(){
 # $2 =   GameVersion (example 1.20.1)
 #                    (if not set version, default get minecraft's latest version)
 papermc_getLatestDLURL() {
-    if [ `wget -qO- https://api.papermc.io/v2/projects | jq ".projects|index(\"$1\")"` != "null" ]; then
+    if [ `curl -s https://api.papermc.io/v2/projects | jq ".projects|index(\"$1\")"` != "null" ]; then
         papermc_url="https://api.papermc.io/v2/projects/"$1
-        papermc_url=$papermc_url"/versions/"${2:-`wget -qO- $papermc_url | jq -r ".versions[-1]"`}
-        papermc_url=$papermc_url"/builds/"`wget -qO- $papermc_url | jq -r ".builds[-1]"`
-        papermc_url=$papermc_url"/downloads/"`wget -qO- $papermc_url | jq -r ".downloads.application.name"`
+        papermc_url=$papermc_url"/versions/"${2:-`curl -s $papermc_url | jq -r ".versions[-1]"`}
+        papermc_url=$papermc_url"/builds/"`curl -s $papermc_url | jq -r ".builds[-1]"`
+        papermc_url=$papermc_url"/downloads/"`curl -s $papermc_url | jq -r ".downloads.application.name"`
         echo "$papermc_url" >&1
     else
         echo "papermc not have "$1 >&2
