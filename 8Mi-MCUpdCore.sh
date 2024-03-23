2abbaf6e145225ec0d36fa82566f6a7f7713c37f
8Mi_Yile <admin@8mi.tech>
2024-03-23 14:13:25 +0800

    Update 8Mi-MCUpdCore.sh: curseforge and modrinth support search mod_loader

--- 8Mi-MCUpdCore.sh
+++ 8Mi-MCUpdCore.sh
@@ -19,30 +19,41 @@ jenkins_getLatestDLURL(){
 # curseforge_getLatestDLURL
 # $1 = * App/Type/Name (example "minecraft/mc-mods/fabric-api")
 # $2 =   Game_Version (example "1.20.1")
+# $2 =   Mod_Loader_Name (example "fabric" "forge" "neoforge")
 curseforge_getLatestDLURL(){
-    cf_json=`curl -s "https://api.cfwidget.com/"$1"?version="$2`
+    cf_json=`curl -s "https://api.cfwidget.com/$1?version=$2&loader=$3"`
     cf_file_id=`echo $cf_json | jq '.download.id'`
     cf_file_name=`echo $cf_json | jq -r '.download.name'`
     if [ $cf_file_id ]; then
         cf_file_id_url=`echo $cf_file_id | sed 's/..../&\//g'`
         if [ ${cf_file_id_url: -1} != \/ ]; then
-        cf_file_id_url=$cf_file_id_url\/
+            cf_file_id_url=$cf_file_id_url\/
         fi
-    cf_file_id_url=`echo $cf_file_id_url | sed 's/\/00/\//g'`
-    cf_file_id_url=`echo $cf_file_id_url | sed 's/\/0/\//g'`
-    cf_file_name=`echo $cf_file_name|sed 's/+/%2B/g'`
-    echo "https://media.forgecdn.net/files/"$cf_file_id_url$cf_file_name >&1
+        cf_file_id_url=`echo $cf_file_id_url | sed 's/\/00/\//g'`
+        cf_file_id_url=`echo $cf_file_id_url | sed 's/\/0/\//g'`
+        cf_file_name=`echo $cf_file_name|sed 's/+/%2B/g'`
+        echo "https://media.forgecdn.net/files/"$cf_file_id_url$cf_file_name >&1
     fi
 }
 
 # modrinth_getLatestDLURL
 # $1 = * Project(Name/ID) (example "fabric-api" or "P7dR8mSH")
 # $2 =   GameVersion (example "1.20.1")
+# $2 =   Mod_Loader_Name (example "fabric" "forge" "neoforge")
 modrinth_getLatestDLURL(){
+    local modrinth_game_version_latest=""
+    local modrinth_url_base=""
+    local modrinth_game_versions=""
+    local modrinth_loaders=""
     if [ !$2 ]; then
-    modrinth_gameversion=`curl -s https://api.modrinth.com/v2/tag/game_version | jq -r .[0].version`
+        modrinth_game_version_latest=`curl -s https://api.modrinth.com/v2/tag/game_version | jq -r .[0].version`
     fi
-    curl -s https://api.modrinth.com/v2/project/$1/version?game_versions=[%22${2:-$modrinth_gameversion}%22] | jq -r .[0].files[0].url >&1
+    modrinth_url_base="https://api.modrinth.com/v2/project/$1/version"
+    modrinth_game_versions="game_versions=[%22${2:-$modrinth_game_version_latest}%22]"
+    if [ "$3" ]; then
+        modrinth_loaders="loaders=[%22${3}%22]"
+    fi
+    curl -s "$modrinth_url_base?$modrinth_game_versions&$modrinth_loaders" | jq -r .[0].files[0].url >&1
 }
 
 # papermc_getLatestDLURL
@@ -50,6 +61,7 @@ modrinth_getLatestDLURL(){
 # $2 =   GameVersion (example 1.20.1)
 #                    (if not set version, default get minecraft's latest version)
 papermc_getLatestDLURL() {
+    local papermc_url=""
     if [ `curl -s https://api.papermc.io/v2/projects | jq ".projects|index(\"$1\")"` != "null" ]; then
         papermc_url="https://api.papermc.io/v2/projects/"$1
         papermc_url=$papermc_url"/versions/"${2:-`curl -s $papermc_url | jq -r ".versions[-1]"`}
@@ -63,12 +75,12 @@ papermc_getLatestDLURL() {
 
 # 8Mi_Download
 # $1 = * FileName (example FabricAPI.jar)
-# $2 =   Download URL (support STDIN) (example https://example.com/fabric-api/fabric-api-1.0-1.20.1.jar) 
+# $2 =   Download URL (support STDIN) (example https://example.com/fabric-api/fabric-api-1.0-1.20.1.jar)
 8Mi_Download(){
     dl_url=`cat /dev/stdin`
     echo "[8Mi-MCUpdCore] 正在通过服务器"` echo ${2:-$dl_url} | awk -F'/' '{print $3}' `" 下载"$1
     wget $WGET_PROGRESS_OPT -O ./_tmp-ci/$1 ${2:-$dl_url}
     if [ 0 -ne $? ]; then
-    echo "[8Mi-MCUpdCore] "$1"下载失败"
+        echo "[8Mi-MCUpdCore] "$1"下载失败"
     fi
 }
